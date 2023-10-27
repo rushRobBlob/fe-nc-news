b3a11b1d7e8bcd9d08421a76378e9da1fd43f47d
rushRobBlob <robbie.rush@ymail.com>
2023-10-27 16:50:20 +0100

    finishing for the day

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -7,35 +7,61 @@ import AddComment from './AddComment.jsx'
 function CommentCard({ article_id }) {
     const [comments, setComments] = useState([]);
     const [isClicked, setIsClicked] = useState(false);
+    const [isPosted, setIsPosted] = useState(false);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [err, setErr] = useState(false);
+
+    const mainUser = 'tickle122';
 
     useEffect(() => {
         axios.get(`https://nc-news-q2aj.onrender.com/api/articles/${article_id}/comments`).then(({ data }) => {
             const { comments } = data;
             setComments(comments);
         })
-    }, []);
+    }, [isPosted, isDeleting]);
 
-    return (
-        <>
-            <ul>
-                {comments.map((comment) => {
-                    return (
-                        <li className="articleComments" key={comment.comment_id}>
-                            <p>User <span className="fullArticleUser">{comment.author}</span> says...</p>
-                            <br />
-                            <p className="commentText">"{comment.body}"</p>
-                            <br />
-                            <span className="commentDate">Date: {dateFormat(comment.created_at)}</span>
-                            <span className="commentVotes">Votes: {comment.votes}</span>
-                        </li>
-                    )
-                })}
-            </ul>
+    const deleteComment = (comment_id) => {
+        setIsDeleting(true);
+        axios.delete(`https://nc-news-q2aj.onrender.com/api/comments/${comment_id}`).then((res) => {
+            console.log(res);
+            setErr(false);
+            setIsDeleting(false);
+        }).catch(() => {
+            setIsDeleting(false);
+            setErr(true);
+
+        })
+    }
+
+    if (isDeleting) { return <div className="deleteContainer"><h3 className="articleDelete">We are deleting your comment....please wait</h3></div> }
+    else {
+        return (
+            <>
+                <ul>
+                    {err ? <p>comment was not deleted...please try again</p> : null}
+                    {comments.map((comment) => {
+                        return (
+                            <li className="articleComments" key={comment.comment_id}>
+                                <p>User <span className="fullArticleUser">{comment.author}</span> says...</p>
+
+                                <p className="commentText">"{comment.body}"</p>
+                                <div className="commentDateVotes">
+                                    <p>Date: {dateFormat(comment.created_at)}</p>
+                                    <p>Votes: {comment.votes}</p>
+                                </div>
+                                {mainUser === comment.author ? <div className="deleteComment"><button onClick={() => deleteComment(comment.comment_id)}>Delete Comment</button></div> : null}
+
+                            </li>
+                        )
+                    })}
+                </ul>
 
 
-            <AddComment article_id={article_id} />
-        </>
-    )
+                <AddComment isPosted={isPosted} setIsPosted={setIsPosted} article_id={article_id} />
+            </>
+        )
+    }
+
 }
 
 export default CommentCard;
